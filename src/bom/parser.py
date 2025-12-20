ad0fdcfe3d7a26ab7668092b7717b6b5c8cc77dd
ebrar <ebraronuk00@gmail.com>
2025-12-20 13:33:49 +0300

    BOM parser için temel iskelet oluşturuldu - çok seviyeli BOM ayrıştırması için BomParser sınıfı tanımlandı - parse, yapı doğrulama, döngü tespiti ve hiyerarşi kurma adımları iskelet olarak eklendi - her metodun sorumluluğu Türkçe docstring ve TODO notları ile açıklandı

--- src/bom/parser.py
+++ src/bom/parser.py
@@ -1,6 +1,22 @@
-def parse_bom(json_data):
-    """
-    Çok seviyeli BOM ayrıştırması için yer tutucu.
-    TODO: ebeveyn-çocuk bağlama, seviye hesaplama, yinelenen ve döngü kontrolleri.
-    """
-    return []
+class BomParser:
+    """Cok seviyeli BOM kayitlarini okuyup yapilandirmak icin iskelet parser sinifi."""
+
+    def parse(self, records):
+        """Girdi kayitlarini alir, dogrular ve agac yapisi icin hazirlar (detaylari TODO)."""
+        # TODO: Kayıt formatı kontrolü, temel alan dogrulama ve sonraki adimlarin zincirlenmesi eklenecek.
+        raise NotImplementedError("TODO: parse implement edilecek")
+
+    def validate_structure(self, nodes):
+        """BOM dugumlerinin yapisal tutarliligini kontrol eder (zorunlu alanlar, seviyeler, tekrarlar)."""
+        # TODO: Zorunlu alan denetimi, level tutarliligi ve parent-child tekrar kontrolu eklenecek.
+        raise NotImplementedError("TODO: validate_structure implement edilecek")
+
+    def detect_cycles(self, nodes):
+        """Parent-child baglantilarinda dongu olup olmadigini tespit eder."""
+        # TODO: Grafik gezintisiyle cycle tespiti eklenip hata/uyari uretilecek.
+        raise NotImplementedError("TODO: detect_cycles implement edilecek")
+
+    def build_hierarchy(self, nodes):
+        """Dogrulanmis dugumleri agac/katman yapisina donusturur."""
+        # TODO: Seviyelere gore hiyerarsi olusturma ve baglanti kurallari uygulanacak.
+        raise NotImplementedError("TODO: build_hierarchy implement edilecek")
